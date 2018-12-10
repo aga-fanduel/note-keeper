ad4c1d7a2f522a9d297a8da059c79ce565a1d465
Aga Magiera <aga.magiera@fanduel.com>
2018-12-10 15:21:19 +0000

    added mIsNewNote private field

--- app/src/main/java/com/example/agamagiera/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/agamagiera/notekeeper/NoteActivity.java
@@ -19,6 +19,7 @@ public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTE_INFO = "com.example.agamagiera.notekeeper.NOTE_INFO";
     private NoteInfo mNote;
+    private boolean mIsNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +41,9 @@ public class NoteActivity extends AppCompatActivity {
         EditText textNoteTitle = findViewById(R.id.text_note_title);
         EditText textNoteText =  findViewById(R.id.text_note_text);
 
-        displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        if(!mIsNewNote) {
+            displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        }
 
     }
 
@@ -55,6 +58,7 @@ public class NoteActivity extends AppCompatActivity {
     private void readDisplayStateValues() {
         Intent intent = getIntent();
         mNote = intent.getParcelableExtra(NOTE_INFO);
+        mIsNewNote = mNote == null;
     }
 
     @Override
